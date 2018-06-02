c81fab96bb943d022ba76f9869188123b2126866
MuhammadAli <idrees.muhammadali@gmail.com>
2018-06-02 08:03:44 +0500

    CodeBase created

---
+++ app/src/main/java/com/example/muhammadali/abcgit/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.muhammadali.abcgit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+//Created codebase for sample project
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
