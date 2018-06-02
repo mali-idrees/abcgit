74d25df161ca80f81459d00d153360ddbbcaecfd
mali-idrees <38298319+mali-idrees@users.noreply.github.com>
2018-06-02 08:14:38 +0500

    Update MainActivity.java

--- app/src/main/java/com/example/muhammadali/abcgit/MainActivity.java
+++ app/src/main/java/com/example/muhammadali/abcgit/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.muhammadali.abcgit;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-
+//file edited from github
 //Created codebase for sample project
 public class MainActivity extends AppCompatActivity {
 
